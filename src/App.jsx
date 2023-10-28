3fa3486696b7e7b56de64201af63028774b242ec
dhanvir007 <dhanvirrodge03@gmail.com>
2023-10-28 15:39:05 +0530

    all-logic-is-completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { FaIndianRupeeSign } from 'react-icons/fa6'
 import './App.css'
 
 function App() {
+  const [billAmount,setBillAmount] = useState(0)
+  const [count, setCount] = useState(1)
+  const [tipPercentage,setTipPercentage] = useState(0)
+  const [tipPerPerson, setTipPerPerson] = useState(0)
+  const [amountPerPerson,setAmountPerPerson] = useState(0)
 
+  
+  
+  const handleChange= (e)=>{
+    const InputValue = parseFloat(e.target.value);
+      if (!isNaN(InputValue)) {
+        setBillAmount(InputValue)
+      }
+   }
+const handleClick = (value) =>{
+     setTipPercentage(value)
+      
+    }
+    
+    // number of person 
+    const increment = () => {
+      setCount(count + 1)
+      
+    }
+    const decrement = () => {
+      if(count > 1){
+      setCount(count - 1)
+    }
+  }
 
-  return (
+    // generate click "GENERATE" 
+    const generateClick = ()=>{
+      const tip = tipPercentage;
+      const person = count ;
+      const bill = billAmount;
+
+     
+   
+    const percentage = tip/100;
+    const calculateTipPerson = bill*percentage;
+    const calculateTipPerPerson = Math.round(calculateTipPerson/person)
+           setTipPerPerson( calculateTipPerPerson);
+  
+    const billperPerson = Math.round( bill/person + calculateTipPerPerson)
+ 
+        setAmountPerPerson(billperPerson)
+        
+      }
+
+      const handleClear =  () =>{  
+        setBillAmount(0);
+        setCount(1);
+        setTipPercentage(0);      
+        setTipPerPerson(0);
+        setAmountPerPerson(0);
+    }
+
+    return (
     <>
       <div className="outer">
         <div>
@@ -20,7 +75,9 @@ function App() {
               <div className="Bill">
                 <label>BILL</label>
                 <div>
-                  <FaIndianRupeeSign /><input type="text" id="bill" placeholder="0"></input>
+                  <FaIndianRupeeSign />
+                  <input type="text" name='bill' id="bill" value={billAmount} placeholder="0" onChange={handleChange}>
+                  </input>
                 </div>
 
               </div>
@@ -28,24 +85,24 @@ function App() {
             <div className='Tip'>
               <p>Selected Tip %</p>
               <div className='percentage'>
-                <button>5%</button>
-                <button>10%</button>
-                <button>15%</button>
-                <button>25%</button>
-                <button>50%</button>
-                <button>75%</button>
+                <button value="5%" onClick={()=>handleClick(5)}>5%</button>
+                <button value="10%" onClick={()=>handleClick(10)}>10%</button>
+                <button value="15%" onClick={()=>handleClick(15)}>15%</button>
+                <button value="25%" onClick={()=>handleClick(25)}>25%</button>
+                <button value="50%" onClick={()=>handleClick(50)}>50%</button>
+                <button value="75%" onClick={()=>handleClick(75)}>75%</button>
               </div>
               <div className='Number'>
                 <p>Number of People</p>
                 <div>
                   <FaUser />
-                  <span><input type="number" id="people"></input></span>
-                  <span><button>+</button></span>
-                  <span id='numberpeople'>0</span>
-                  <span><button>-</button></span>
+                  <span><input type="number" id="people" value={count} onChange={(e)=>setCount(parseFloat(e.target.value))} /></span>
+                  <span><button onClick={increment}>+</button></span>
+                  <span id='numberpeople'>{count}</span>
+                  <span><button onClick={decrement}>-</button></span>
                 </div>
               </div>
-              <button className='buttons'>Generate Bill</button>
+              <button className='buttons' onClick={generateClick}>Generate Bill</button>
             </div>
 
           </div>
@@ -53,21 +110,21 @@ function App() {
             <div>
               <div className="amount">
                 <p>Tip Amount <br />
-                <span>/ person</span>
+                  <span>/ person</span>
                 </p>
-                <p> <FaIndianRupeeSign /> 0.00</p>
+                <p> <FaIndianRupeeSign />{tipPerPerson}</p>
 
               </div>
               <div className="amount">
                 <p>Total <br />
                   <span>/ person</span>
                 </p>
-                <p> <FaIndianRupeeSign /> 0.00</p>
+                <p> <FaIndianRupeeSign /> {amountPerPerson}</p>
 
               </div>
             </div>
             <div>
-              <button  className='button'>Reset</button>
+              <button className='button' onClick={handleClear}>Reset</button>
             </div>
           </div>
         </div>
